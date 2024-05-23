a3aed87d4683251e0fbe7cd793923de1346b5991
Caio Souza <caio.souza106@gmail.com>
2024-05-23 16:24:54 -0300

    Adding checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -57,9 +57,15 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
-        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-        if (chessMatch.getCheck()){
-            System.out.print(ANSI_RED + "CHECK!" + ANSI_RESET);
+        if (!chessMatch.getCheckMate()) {
+            System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+            if (chessMatch.getCheck()) {
+                System.out.print(ANSI_RED + "CHECK!" + ANSI_RESET);
+            }
+        }
+        else {
+            System.out.println(ANSI_RED + "CHECKMATE!" + ANSI_RESET);
+            System.out.println(chessMatch.getCurrentPlayer() + " wins!");
         }
     }
 
